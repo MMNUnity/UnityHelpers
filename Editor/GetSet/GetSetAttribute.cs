51d6fd651ba30244fd3c73bf9a32a093375a19de
Mrn <mirren91@hotmail.com>
2020-04-01 13:03:26 +0100

    Helpers for serliaziable getters and setters

---
+++ Editor/GetSet/GetSetAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetSetAttribute : PropertyAttribute
+{
+    public readonly string name;
+    public bool dirty;
+    public GetSetAttribute(string name)
+    {
+        this.name = name;
+    }
+}
