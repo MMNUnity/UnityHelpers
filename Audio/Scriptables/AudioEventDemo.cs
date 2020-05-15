fc6f28da3bb73c02224f4a3a820c4d95ee11db86
MM-N <mirren91@hotmail.com>
2020-05-15 11:29:50 +0100

    Audio Scriptable Test

---
+++ Audio/Scriptables/AudioEventDemo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using Random = UnityEngine.Random;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "AudioEvents/Demo")]
+public class AudioEventDemo : AudioEvent
+{
+    public AudioClip[] audioClips;
+
+    //public RangedFloat volume;
+    public float volumeMin;
+    public float volumeMax;
+    public float pitchMin;
+    public float pitchMax;
+
+    //[MinMaxRange()]
+
+    public override void Play(AudioSource audioSource)
+    {
+        if (audioClips.Length == 0) return;
+
+        audioSource.clip = audioClips[Random.Range(0, audioClips.Length)];
+        audioSource.volume = Random.Range(volumeMin, volumeMax);
+        audioSource.pitch = Random.Range(pitchMin, pitchMax);
+
+        audioSource.Play();
+    }
+}
---
+++ Audio/Scriptables/ScriptableObj.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class ScriptableObj : ScriptableObject
+{
+    public int variableUno;
+}
